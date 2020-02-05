138f35b1789971437f1080933fc72bc00916d837
Adsfk <alv_morenitu@hotmail.com>
2020-02-05 14:34:23 +0000

    Refactor - test names changed

--- src/main/test/enigma/RotorGroupTest.java
+++ src/main/test/enigma/RotorGroupTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class RotorGroupTest {
     @Test
-    public void should_pass_by_reflectors_returning_G() {
+    public void should_pass_by_rotors_returning_G() {
         Rotor r0 = new Rotor("0");
         Rotor r1 = new Rotor("1");
         Rotor r2 = new Rotor("2");
@@ -15,11 +15,12 @@ public class RotorGroupTest {
     }
 
     @Test
-    public void should_pass_by_reflectors_returning_H() {
+    public void should_pass_by_rotors_returning_H() {
         Rotor r0 = new Rotor("0");
         Rotor r1 = new Rotor("1");
         Rotor r2 = new Rotor("2");
         RotorGroup rotors = new RotorGroup(r0,r1,r2,null);
         assertEquals(rotors.code("B"),"H");
     }
+
 }
