0abcfa338504069ecf8c61bebb9743c2073b1deb
Adsfk <alv_morenitu@hotmail.com>
2020-02-05 14:35:43 +0000

    Red test - should_pass_by_rotors_returning_C

--- src/main/test/enigma/RotorGroupTest.java
+++ src/main/test/enigma/RotorGroupTest.java
@@ -23,4 +23,12 @@ public class RotorGroupTest {
         assertEquals(rotors.code("B"),"H");
     }
 
+    @Test
+    public void should_pass_by_rotors_returning_C() {
+        Rotor r0 = new Rotor("3");
+        Rotor r1 = new Rotor("1");
+        Rotor r2 = new Rotor("2");
+        RotorGroup rotors = new RotorGroup(r0,r1,r2,null);
+        assertEquals(rotors.code("A"),"C");
+    }
 }
