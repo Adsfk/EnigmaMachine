6640db8cbcb8dcf0c8e322349b60e2c07f6b11b0
Adsfk <alv_morenitu@hotmail.com>
2020-02-05 14:51:17 +0000

    Refactor - tests joined

--- src/main/test/enigma/RotorGroupTest.java
+++ src/main/test/enigma/RotorGroupTest.java
@@ -6,22 +6,15 @@ import static org.junit.Assert.*;
 
 public class RotorGroupTest {
     @Test
-    public void should_pass_by_rotors_returning_G() {
+    public void should_pass_by_rotors() {
         Rotor r0 = new Rotor("0");
         Rotor r1 = new Rotor("1");
         Rotor r2 = new Rotor("2");
         RotorGroup rotors = new RotorGroup(r0,r1,r2,null);
         assertEquals("G",rotors.forward("A"));
-    }
-
-    @Test
-    public void should_pass_by_rotors_returning_H() {
-        Rotor r0 = new Rotor("0");
-        Rotor r1 = new Rotor("1");
-        Rotor r2 = new Rotor("2");
-        RotorGroup rotors = new RotorGroup(r0,r1,r2,null);
         assertEquals("H",rotors.forward("B"));
     }
+
 
     @Test
     public void should_pass_by_rotors_returning_C() {
