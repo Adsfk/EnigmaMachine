aa25876fdfada03e35d04a1b7c4a019b8e25d35d
Adsfk <alv_morenitu@hotmail.com>
2020-02-05 11:43:21 +0000

    Refactor - Set rotor type

--- src/main/java/enigma/Rotor.java
+++ src/main/java/enigma/Rotor.java
@@ -2,6 +2,12 @@ package enigma;
 
 public class Rotor {
 
+    private String type;
+
+    public Rotor(String type) {
+        this.type = type;
+    }
+
     public String translate(String letter) {
         return "B";
     }
--- src/main/test/enigma/RotorTest.java
+++ src/main/test/enigma/RotorTest.java
@@ -8,7 +8,9 @@ public class RotorTest {
 
     @Test
     public void should_change_letter_a() {
-        Rotor rotor = new Rotor();
+        Rotor rotor = new Rotor("I");
         assertEquals("B",rotor.translate("A"));
     }
+
+
 }
