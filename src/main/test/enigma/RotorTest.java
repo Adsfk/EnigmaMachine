1ad2543286ffe914ebd21bf7a9ca0bf8f12a68b9
Adsfk <alv_morenitu@hotmail.com>
2020-02-05 11:45:44 +0000

    Refactor - test name changed

--- src/main/test/enigma/RotorTest.java
+++ src/main/test/enigma/RotorTest.java
@@ -6,11 +6,11 @@ import static org.junit.Assert.*;
 
 public class RotorTest {
 
+    private Rotor rotor = new Rotor("I");
+
     @Test
-    public void should_change_letter_a() {
-        Rotor rotor = new Rotor("I");
+    public void should_change_letter_a_by_b() {
         assertEquals("B",rotor.translate("A"));
     }
-
 
 }
